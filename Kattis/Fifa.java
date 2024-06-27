2fc5ba5b3cbf94a10703f0e8132a4a53f0f020be
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-06-28 01:30:07 +1000

    Framtíðar FIFA

--- Kattis/Fifa.java
+++ Kattis/Fifa.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Fifa {
     //Framtíðar FIFA
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 2022 + number of improvements since you were frozen / number of improvements the game receives every year
-        System.out.println(2022 + (sc.nextInt()/sc.nextInt()));
+        try (Scanner sc = new Scanner(System.in)) {
+            // 2022 + number of improvements since you were frozen / number of improvements the game receives every year
+            System.out.println(2022 + (sc.nextInt()/sc.nextInt()));
+        }
     }
 }
