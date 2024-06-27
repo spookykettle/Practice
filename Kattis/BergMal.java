6a32b4935d8a2cf6c757807337a8d76d68c6025e
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-06-28 00:52:18 +1000

    First commit

---
+++ Kattis/BergMal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BergMal {
+
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) 
+        {
+            try 
+            {
+                System.out.println(sc.nextLine());
+            }
+            catch (Exception e)
+            {
+                System.out.println(sc.nextFloat());
+            }
+        }
+        
+    }
+}
+
+//  System.out.print();
