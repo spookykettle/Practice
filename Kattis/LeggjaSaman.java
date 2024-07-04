ed629e7b55d16c3453bd4357e3fc4abef7963032
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-05 01:18:31 +1000

    Leggja saman

---
+++ Kattis/LeggjaSaman.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class LeggjaSaman {
+// Leggja saman
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in))
+        {
+            System.out.println(input.nextInt()+input.nextInt());
+        }
+    }
+}
