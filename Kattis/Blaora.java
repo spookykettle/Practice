dae76aac8cc007227cdcc721f3b4ef3806f22b21
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-04 21:17:59 +1000

    Blaðra

---
+++ Kattis/Blaora.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Blaora {
+    public static void main(String[] args) {
+        // Blaðra
+        
+        try (Scanner input = new Scanner(System.in))
+        {
+            String inputValue = input.nextLine();
+            String[] num = inputValue.split(" ");
+
+            int v = Integer.parseInt(String.valueOf(num[0]));
+            int a = Integer.parseInt(String.valueOf(num[1]));
+            int t = Integer.parseInt(String.valueOf(num[2]));
+
+            double d = (v*t) + ((0.5*a)*(t*t));
+            System.out.printf("%.9f", d);
+            
+        }
+    }
+}
