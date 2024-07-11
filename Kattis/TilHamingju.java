54dd8bb7f169ba7eaaaafc9f691417cc9e41e18c
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-12 02:32:39 +1000

    Til hamingju

---
+++ Kattis/TilHamingju.java
@@ -0,0 +1,6 @@
+// Til hamingju
+public class TilHamingju {
+    public static void main(String[] args) {
+        System.out.println("TIL HAMINGJU MED AFMAELID FORRITUNARKEPPNI FRAMHALDSSKOLANNA!\n");
+    }
+}
