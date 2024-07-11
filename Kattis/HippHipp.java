960522a03aa60ad891b127fe0faa187e1ae94727
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-12 02:30:36 +1000

    Hipp Hipp

---
+++ Kattis/HippHipp.java
@@ -0,0 +1,9 @@
+// Hipp Hipp
+public class HippHipp {
+    public static void main(String[] args) {
+        for(int i = 0; i < 20; i++)
+        {
+            System.out.println("Hipp hipp hurra!");
+        }
+    }
+}
