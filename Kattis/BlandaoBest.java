d43a63897cb473395a322aa474b3e7a3b965c19f
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-12 02:28:34 +1000

    Blandað Best

--- Kattis/BlandaoBest.java
+++ Kattis/BlandaoBest.java
@@ -1,40 +1,40 @@
-//Blandað Best
+// Blandað Best
 
 import java.util.Scanner;
 
 public class BlandaoBest {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            int n = input.nextInt(); // the number of meat types KFFÍ is offering today.
+            @SuppressWarnings("unused") // because we don't actually use dummy varaible. The wiggly line under bothered me.
+            String dummy = input.nextLine(); // This is to clear input buffer
 
-        int n = input.nextInt(); // the number of meat types KFFÍ is offering today.
-        @SuppressWarnings("unused") // because we don't actually use dummy varaible. The wiggly line under bothered me.
-        String dummy = input.nextLine(); // This is to clear input buffer
+            String[] food = new String[n];
+            String foodmix = "";
 
-        String[] food = new String[n];
-        String foodmix = "";
-
-        for (int i = 0; i < n; i++)
-        { 
-            food[i] = input.nextLine();
-            
-            // use .equals when comparing object from Class string 
-            if (food[i].equals("kjuklingur"))
-            {
-                foodmix += "g";
+            for (int i = 0; i < n; i++)
+            { 
+                food[i] = input.nextLine();
+                
+                // use .equals when comparing object from Class string 
+                if (food[i].equals("kjuklingur"))
+                {
+                    foodmix += "g";
+                }
+                else if(food[i].equals("nautakjot"))
+                {
+                    foodmix += "j";
+                }
             }
-            else if(food[i].equals("nautakjot"))
-            {
-                foodmix += "j";
-            }
-        }
 
-        if (foodmix.contains("g") && foodmix.contains("j"))
-        {
-            System.out.println("blandad best");
-        }
-        else
-        {
-            System.out.println(food[0]);
+            if (foodmix.contains("g") && foodmix.contains("j"))
+            {
+                System.out.println("blandad best");
+            }
+            else
+            {
+                System.out.println(food[0]);
+            }
         }
         
     }
