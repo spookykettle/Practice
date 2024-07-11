445534283dc6f4637d8e7e0e569f5f1c441c65b5
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-12 01:34:10 +1000

    Quadrant Selection

---
+++ Kattis/QuadrantSelection.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//Quadrant Selection
+
+public class QuadrantSelection {
+    public static void main(String[] args)
+    {
+        try (Scanner input = new Scanner(System.in)) 
+        {
+            int x = input.nextInt();
+            int y = input.nextInt();
+            int output = 0;
+
+            if (x > 0 && y > 0) output = 1;
+            else if (x < 0 && y > 0) output = 2;
+            else if (x < 0 && y < 0) output = 3;
+            else if (x > 0 && y < 0) output = 4;
+
+            System.out.println(output);
+        }
+    }
+}
