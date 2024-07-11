fa2b57317e5d568bbe2c07ed54c950c4d5a4ea59
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-12 01:25:02 +1000

    Autori

---
+++ Kattis/Autori.java
@@ -0,0 +1,23 @@
+// Autori
+
+import java.util.Scanner;
+
+public class Autori 
+{
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) 
+        {
+            String inputText = input.nextLine();
+
+            String[] textArray = inputText.split("-");
+
+            for(String each:textArray)
+            {
+                System.out.print(each.charAt(0));
+            }
+            System.out.println(); // to get rid of %
+        }
+
+    }
+
+}
