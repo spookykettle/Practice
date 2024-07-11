b5f51af0d70b56dcd710f3be1913b008dbc7caa0
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-12 02:48:13 +1000

    Jack-O'-Lantern Juxtaposition

---
+++ Kattis/JackOLantern.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Jack-O'-Lantern Juxtaposition
+public class JackOLantern {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String inputInt = input.nextLine();
+        String[] numArray = inputInt.split(" ");
+
+        System.out.println(Integer.parseInt(numArray[0])*Integer.parseInt(numArray[1])*Integer.parseInt(numArray[2]));
+        
+    }
+}
