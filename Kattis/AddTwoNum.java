84073ec7d8edb44b381a54d3b677adf805faf911
Pun Rojanamontien <pun.rojanamontien@gmail.com>
2024-07-12 16:37:58 +1000

    Add Two Numbers, Two-sum

---
+++ Kattis/AddTwoNum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Add Two Numbers
+public class AddTwoNum {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        String num[]= x.nextLine().split(" "); // this could be more than two num
+
+        System.out.println(Integer.parseInt(num[0])+Integer.parseInt(num[1])); // add index 0 and 1 together
+
+        x.close();
+    }
+}
