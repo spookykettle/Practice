31882aa51e2714dda9661860706b1fec2702922f
spookykettle <92515499+spookykettle@users.noreply.github.com>
2025-07-09 15:44:44 +1000

    Triangle area

--- Kattis/TriangleArea.java
+++ Kattis/TriangleArea.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class TriangleArea {
     public static void main(String[] args) {
-         Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         
         String[] inputInt = input.nextLine().split(" ");
 
